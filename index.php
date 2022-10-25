60c10cf19cebb065aea8f11b2253b831e5d38c07
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-10-25 21:22:28 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello!<br>";
+
+    $nama = "Amalia Putri Damayanti";
+    $umur = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaKakak = "Mayang";
+    $umurKakak = "28";
+
+    echo "Nama kakak saya <strong>$namaKakak</strong>, saya berusia $umurKakak tahun.<br>";
+
+    $selisihUmur = $umur - $umurKakak;
+    echo "selisih usia saya dan kakak saya adalah $selisihUmur tahun."
+
+?>
